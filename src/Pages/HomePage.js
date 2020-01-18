16d17a717aba6904d51539684cc8dfbff10856b1
vtruo009 <43657607+vtruo009@users.noreply.github.com>
2020-01-18 15:55:45 -0800

    Testing layout

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { WiBarometer } from "react-icons/wi"
 
 class HomePage extends Component {
     render() {
         return (
             <div>
+                <WiBarometer size = {20*14} /> 
                 <h1>Home Page</h1>
             </div>
         );
