0e7f281a19db0e7ac6df19b7886de0bbacd43a78
vtruo009 <43657607+vtruo009@users.noreply.github.com>
2020-01-18 17:10:19 -0800

    Imported all needed icons

--- src/Pages/WeatherPage.js
+++ src/Pages/WeatherPage.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { TiWeatherCloudy } from "react-icons/ti"
+import { WiWindy } from "react-icons/wi"
+import { WiCloudy } from "react-icons/wi"
+import { WiCloudyWindy } from "react-icons/wi"
+import { WiRain } from "react-icons/wi"
+import { WiRainWind } from "react-icons/wi"
+import { WiSnow } from "react-icons/wi"
+import { WiSnowWind } from "react-icons/wi"
+import { WiSprinkle } from "react-icons/wi"
+import { WiShowers } from "react-icons/wi"
+import { WiThunderstorm } from "react-icons/wi"
+import { WiFog} from "react-icons/wi"
+import { WiHail } from "react-icons/wi"
+import { WiDegrees } from "react-icons/wi"
+import { WiDaySunny} from "react-icons/wi"
+import { WiNightClear } from "react-icons/wi"
+
 
 class WeatherPage extends Component {
     render() {
         return (
-            <div>
+            <div className = "WeatherDiv">
                <h1>Weather Page</h1>
                <div className = "row">
                     <div className = "col">
-                        <TiWeatherCloudy size={20*14}/>
+                        <WiCloudy size={20*14}/>
                         <br></br>
                         <font size = "7"> Temperature </font>
                         <br/>
